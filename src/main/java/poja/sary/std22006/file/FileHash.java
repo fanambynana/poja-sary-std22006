758f4260c64f1cb998fd39a5925331fdd3aa0651
fanambynana <hei.fanambinana@gmail.com>
2024-02-21 14:13:53 +0300

    infra: edit package name & install openimaj dep

--- src/main/java/poja/sary/std22006/file/FileHash.java
+++
@@ -1,6 +0,0 @@
-package poja.sary.std22006.file;
-
-import poja.sary.std22006.PojaGenerated;
-
-@PojaGenerated
-public record FileHash(FileHashAlgorithm algorithm, String value) {}
--- src/main/java/poja/sary/std22006/file/FileHashAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package poja.sary.std22006.file;
-
-import poja.sary.std22006.PojaGenerated;
-
-@PojaGenerated
-public enum FileHashAlgorithm {
-  SHA256,
-  NONE
-}
